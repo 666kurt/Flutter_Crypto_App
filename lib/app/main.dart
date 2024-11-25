cf7431772a157d48a79b53c1ab99b5a77d1273cb
Maxim <incentivedie@gmail.com>
2024-11-25 13:27:20 +0400

    Fix UI

--- lib/app/main.dart
+++ lib/app/main.dart
@@ -9,6 +9,9 @@ void main() {
 class CryptoApp extends StatelessWidget {
   const CryptoApp({super.key});
 
+  final url =
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h&locale=en";
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
